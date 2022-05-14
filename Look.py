67075c9a3420665daca5f6344ec7c81c51682997
Luis Jean <luisjeanknowsbest@gmail.com>
2022-05-13 22:43:38 -0400

    Update Look.py

---
+++ Look.py
@@ -0,0 +1,9 @@
+app.layout = html.Div(
+	children=[
+		html.H1(children="1 Analytics",),
+		html.P(
+			children="Analyze the behavior of x"
+			" and the number of x currently"
+			" between y and z".
+			),
+			dcc.Graph
